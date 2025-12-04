fdc4b1b68fd7b2592f8a33a832a1b9c9a99ba05a
Nicola <nic00la.kaleta@gmail.com>
2025-12-04 22:19:16 +0100

    first commit

---
+++ algortymeuklidesa/src/components/Form.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+      <h1>Algorytm Euklidesa</h1>
+      <h3>Obliczanie NWD - Największa Wspólna Wielokrotność</h3>
+      <form>
+        <label>Podaj 1 liczbę</label>
+        <input type="text" />
+        <label>Podaj 2 liczbę</label>
+        <input type="text" />
+        <button>Oblicz</button>
+      </form>
+    </div>
+  )
+}
+
+export default Form
