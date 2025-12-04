523803e7fb322a7f0a73f7dc731fdee58482dc7d
Nicola <nic00la.kaleta@gmail.com>
2025-12-04 22:48:32 +0100

    algorytm euklidesa w console log

--- algortymeuklidesa/src/components/Form.jsx
+++ algortymeuklidesa/src/components/Form.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
 
 const Form = () => {
+
+//   const [liczbaA, setLiczbaA] = useState('');
+//   const [liczbaB, setLiczbaB] = useState('');
+//   const liczbaAsNumber = Number(20);
+
+  // Oryginalne wyniki (do console.log)
+  let a = 48;
+  let b = 18;
+
+  let liczbaA = a;
+  let liczbaB = b;
+
+  while (liczbaB != 0) {
+    let reszta = liczbaA % liczbaB;
+    liczbaA = liczbaB;
+    liczbaB = reszta
+  }
+ 
+  console.log(`NWD(${a}, ${b}): ${liczbaA}`);
+
   return (
     <div>
       <h1>Algorytm Euklidesa</h1>
       <h3>Obliczanie NWD - Największa Wspólna Wielokrotność</h3>
       <form>
         <label>Podaj 1 liczbę</label>
-        <input type="text" />
+        <input type="text" 
+                value={liczbaA}/>
         <label>Podaj 2 liczbę</label>
-        <input type="text" />
+        <input type="text" 
+                value={liczbaB}/>
         <button>Oblicz</button>
       </form>
 
